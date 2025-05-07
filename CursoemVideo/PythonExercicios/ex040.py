3b84e1387f75c12aa28518232cc7a5d914d6a009
PedroZacaz <pedroproli@hotmail.com>
2025-05-07 17:17:03 -0300

    Exercício 40 da aula 12. Clássico...

--- CursoemVideo/PythonExercicios/ex040.py
+++ CursoemVideo/PythonExercicios/ex040.py
@@ -1,14 +1,14 @@
-from statistics import median
-
 n1 = float(input('Digite a 1° nota: '))
 n2 = float(input('Digite a 2° nota: '))
 media = (n1+n2)/2
 
 if media < 5:
-    print(f'Sua média {media} está abaixo de 5: REPROVADO')
+    print(f'Sua média {media:.1f} está abaixo de 5: REPROVADO')
 elif media >= 7:
-    print(f'Sua média {media} está ótima, você está: APROVADO')
-elif media >= 5:
-    print(f'A média {media} está na zona de: RECUPERAÇÃO')
-elif media <= 6.9:
-    print(f'A média {media} está na zona de: RECUPERAÇÃO')
+    print(f'Sua média {media:.1f} está ótima, você está: APROVADO')
+elif 7 > media >= 5:
+    print(f'A média {media:.1f} está na zona de: RECUPERAÇÃO')
+
+#era só usar a matemática
+'''elif media <= 6.9:
+    print(f'A média {media:.1f} está na zona de: RECUPERAÇÃO')'''
