57b9665ba572afc2f0953efe5a14253f529e2e6e
PedroZacaz <pedroproli@hotmail.com>
2025-05-07 16:25:14 -0300

    Exercício 36 da aula 12

--- CursoemVideo/PythonExercicios/ex036.py
+++ CursoemVideo/PythonExercicios/ex036.py
@@ -5,15 +5,14 @@ print('-='*15)
 print('Entenda se será possível fazer um empréstimo para comprar a sua casa')
 casa = float(input('Qual o valor da CASA: '))
 sal = float(input('Quanto de salário você recebe: '))
-ano = float(input('Em quantos anos pretende pagar a casa: '))
+ano = int(input('Em quantos anos pretende pagar a casa: '))
 
-prestacao = casa / ano
-mensal = (prestacao / 12)
-calc = mensal - mensal * 0.30
+prestacao = casa / (ano * 12)
+minimo = sal * 30 / 100
 
-print(f'O valor da prestação é {prestacao:.2f}')
+print(f'Para pagar uma casa de R${casa:.2f} em {ano} anos a prestação será de R$ {prestacao:.2f}')
 
-if calc > sal:
-    print(f'Você não vai poder fazer o emprétimo, o seu salário não permite.')
-elif calc < sal:
-    print(f'Você pode fazer o empréstimo, o valor será por ano R$ {prestacao:.2f} e a mensalidade é R$ {mensal:.2f}')
+if prestacao <= minimo:
+    print(f'Empréstimo pode ser CONCEDIDO!')
+else:
+    print('Empréstimo NEGADO!')
