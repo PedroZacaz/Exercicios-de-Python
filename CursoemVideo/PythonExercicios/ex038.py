11292e9da9471db53158342f902237b30ca2c443
PedroZacaz <pedroproli@hotmail.com>
2025-05-07 16:48:54 -0300

    Exercício 38 da aula 12

--- CursoemVideo/PythonExercicios/ex038.py
+++ CursoemVideo/PythonExercicios/ex038.py
@@ -1,12 +1,13 @@
 n1 = int(input('Digite o 1° Número: '))
 n2 = int(input('Digite o 2° Número: '))
+#Cor
 azul = '\33[34m'
 verde = '\33[32m'
 limpar = '\33[m'
 
 if n1 < n2:
-    print(f'O {verde}2° valor{limpar} que é{azul} {n2}{limpar}, é maior.')
+    print(f'O {verde}2° valor que é {n2}{limpar} é maior.')
 elif n2 < n1:
-    print(f'O {verde}1° valor{limpar} que é{azul} {n1}{limpar}, é maior>')
+    print(f'O {azul}1° valor que é {n1}{limpar} é maior.')
 else:
     print(f'Os valores são iguais.')
