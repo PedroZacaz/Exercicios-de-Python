685e3defe5c275b63a955cdffde4a9134bff1868
PedroZacaz <pedroproli@hotmail.com>
2025-05-09 15:43:10 -0300

    Aula 13

---
+++ CursoemVideo/Aula/aula013.py
@@ -0,0 +1,16 @@
+'''i = int(input('início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')'''
+
+s = 0
+
+for c in range (0, 4):
+    n = int(input('Digite um valor: '))
+    #Jeito python
+    s += n
+    #Jeito normal
+    #s = s + n Muito normal
+print(f'O somatório de tudo é: {s}')
