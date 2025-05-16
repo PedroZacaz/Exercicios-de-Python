1c4de34051153b2ae47927d7c02b4a070a2dffbe
PedroZacaz <pedroproli@hotmail.com>
2025-05-16 14:19:30 -0300

    Ex 47 da Aula 13

--- CursoemVideo/PythonExercicios/ex047.py
+++ CursoemVideo/PythonExercicios/ex047.py
@@ -1,8 +1,13 @@
-from time import sleep
+'''from time import sleep
 
 print('\033[0;31;40m CONTAGEM DOS NÚMEROS PARES ATÉ 50 \033[0m')
 for c in range(1, 50+1):
     par = c % 2
     if par == 0:
         sleep(0.3)
-        print(c)
+        print(c, end=' ') #O end faz com que o código execute na mesma linha'''
+
+#Feito no video
+for n in range(2, 51, 2):
+    print(n, end=' ')
+print('Acabou')
